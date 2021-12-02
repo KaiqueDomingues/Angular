2ffee7ffc3b26106722423c2e430f056769a31bf
Kaique-Domingues <kaique_ray258@hotmail.com>
2021-12-02 10:55:27 -0300

    Atualização

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+  cor = 'vermelho';
+  nome = 'Kaique';
+  valorDigitado: any;
+  resultado!: boolean;;
+
+  // botaoClick(){
+  //   alert("Uma mensagem");
+  //   this.cor = (this.cor == "azul" ? "vermelho": "azul");
+  // }
+
+
+  //inputKeyUp(input:any){
+  //console.log(input.target.value);
+  // this.valorDigitado = input.target.value;
+  //console.log(this.valorDigitado);
+  //}
+
+  getValor(){
+    return ('BRQ');
+  }
+
+  validaCPF(){
+    console.log(this.testaCPF(this.valorDigitado))
+    this.resultado = this.testaCPF(this.valorDigitado)
+  }
+
+  testaCPF(strCPF: string) {
+    
+    var Soma;
+    var Resto;
+    Soma = 0;
+    if (strCPF == "00000000000") return false;
+
+    for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+    return true;
+  }
 }
