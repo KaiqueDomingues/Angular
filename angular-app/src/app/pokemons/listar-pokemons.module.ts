2cde0c409e10625d854bf66873bdb06ba6842c29
Kaique-Domingues <kaique_ray258@hotmail.com>
2021-12-08 18:05:16 -0300

    Atualizado shared

--- angular-app/src/app/pokemons/listar-pokemons.module.ts
+++ angular-app/src/app/pokemons/listar-pokemons.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DetalhesPokemonsComponent } from './detalhes-pokemons/detalhes-pokemons.component';
 import { ListarPokemonsComponent } from './listar-pokemons/listar-pokemons.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -13,7 +14,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    //HttpClientModule,
+    SharedModule
   ],
   exports: [
     ListarPokemonsComponent
--- angular-app/src/app/usuarios/usuarios.module.ts
+++ angular-app/src/app/usuarios/usuarios.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarUsuariosComponent } from './listar-usuarios/listar-usuarios.component';
 import { DetalheUsuarioComponent } from './detalhe-usuario/detalhe-usuario.component';
-import { CpfPipe } from './cpf.pipe';
-import { GrifaTextoDirective } from './grifa-texto.directive';
+//import { CpfPipe } from './cpf.pipe';
+//import { GrifaTextoDirective } from './grifa-texto.directive';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -11,12 +12,12 @@ import { GrifaTextoDirective } from './grifa-texto.directive';
   declarations: [
     ListarUsuariosComponent,
     DetalheUsuarioComponent,
-    CpfPipe,
-    GrifaTextoDirective
+    //CpfPipe,
+    //GrifaTextoDirective
   ],
   imports: [
     CommonModule,
-    
+    SharedModule
   ],
   exports:[
     ListarUsuariosComponent
